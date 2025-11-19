d2aab0d9c2ab2ed6bfc026c170f2bb2560a19aff
Asmaa Khaled <assmaakhaled609@gmail.com>
2025-11-19 10:09:10 +0200

    fixed color

--- dubox-frontend/src/environments/environment.ts
+++ dubox-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44333/api', // Update with your backend URL
+  apiUrl: 'https://localhost:7098/api', // Update with your backend URL
   apiVersion: 'v1',
 };
 
